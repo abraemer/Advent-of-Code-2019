6452834a3575bc627d5d1ec7c521e017c1b86b32
NobodysHero <11058200+NobodysHero@users.noreply.github.com>
2019-12-11 20:58:13 +0100

    Solution for day 09 - forgot to write the oneliner into his file

--- day09.lisp
+++ day09.lisp
@@ -2,3 +2,7 @@
 
 (in-package :aoc2019.day09)
 
+(defun day09 ()
+  (values
+   (first (prog-outputs (execute-program! (load-program (first (read-puzzlefile 9)) :inputs '(1)))))
+   (first (prog-outputs (execute-program! (load-program (first (read-puzzlefile 9)) :inputs '(2)))))))
