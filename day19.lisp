82b4212142973cb3ef5629c81b4180664a7e9808
Adrian Braemer <11058200+abraemer@users.noreply.github.com>
2021-01-05 14:15:39 +0100

    (Day19) Do not attempt to read a file on load time

--- day19.lisp
+++ day19.lisp
@@ -2,20 +2,20 @@
 
 (in-package :aoc2019.day19)
 
-(defvar *code* (first (read-puzzlefile 19)))
+(defvar *code*)
 
 (defun position-affected-p (x y)
   (= 1
      (first (prog-outputs (execute-program! (load-program
 					     *code*
 					     :inputs (list x y)))))))
-
 (defun day19 ()
-  (loop
-     :for x :from 0 :below 50
-     :sum (loop
+  (let ((*code* (first (read-puzzlefile 19))))
+    (loop
+      :for x :from 0 :below 50
+      :sum (loop
 	     :for y :from 0 :below 50
-	     :counting (position-affected-p x y))))
+	     :counting (position-affected-p x y)))))
 
 (defun find-affected-start (y &optional (start-x 0))
   (loop
@@ -24,9 +24,10 @@
      :finally (return x)))
 
 (defun day19-part2 (&optional (size 100))
-  (loop
-     :with edge-length := (- size 1)
-     :for y :from size
-     :for x := (find-affected-start y) :then (find-affected-start y x)
-     :until (position-affected-p (+ x edge-length) (- y edge-length))
-     :finally (return (+ (- y edge-length) (* 10000 x)))))
+  (let ((*code* (first (read-puzzlefile 19))))
+    (loop
+      :with edge-length := (- size 1)
+      :for y :from size
+      :for x := (find-affected-start y) :then (find-affected-start y x)
+      :until (position-affected-p (+ x edge-length) (- y edge-length))
+      :finally (return (+ (- y edge-length) (* 10000 x))))))
