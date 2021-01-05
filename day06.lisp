2d70700fcfc791f261a670c8479cd82caca9eeee
Adrian Braemer <11058200+abraemer@users.noreply.github.com>
2021-01-05 14:00:22 +0100

    Fixed an issue with the symbols
    The problem here was, that an external call of (day06:day06) did not produce the correct result.
    While not strictly supported (the symbol 'day06 is not exported) this surprised me.
    The issue was that for the second part of the solution, I try to lookup 2 symbols in the hash table. When run from another package the symbols withing the hash table would belong to the 'other' package while the code tries to lookup symbols from the 'day06'-package.
    The fix is to make all symbols keywords, s.t. that they belong to the same package.

--- day06.lisp
+++ day06.lisp
@@ -4,27 +4,29 @@
 
 (defun parse-input (&optional (file (puzzlefile 6)))
   (loop-line-by-line file
-     :with map := (make-hash-table)
-     :for (center orbitee) := (mapcar 'read-from-string (split-seq line #\)))
+    :with map := (make-hash-table)
+    :for (center orbitee) := (mapcar (lambda (name) (intern name 'keyword))
+				     (split-seq line #\)))
      :do (setf (gethash orbitee map) center)
      :finally (return map)))
 
 (defun orbit-count (map object)
   (loop
-     :for current := (gethash object map nil) :then (gethash current map nil)
-     :while current
-     :count t))
+    :for current := (gethash object map nil) :then (gethash current map nil)
+    :while current
+    :count t))
 
 (defun build-hierarchy (map start)
   (loop
-     :for current := (gethash start map nil) :then (gethash current map nil)
-     :while current
-     :collect current))
+    :for current := (gethash start map nil) :then (gethash current map nil)
+    :while current
+    :collect current))
 
 (defun day06 ()
   (let ((map (parse-input)))
     (format t "There are ~a total orbits in the map.~%It takes ~a transfers to get to santa."
 	    (loop
-	       :for object :being :the :hash-keys :of map
-	       :sum (orbit-count map object))
-	    (length (nset-exclusive-or (build-hierarchy map 'san) (build-hierarchy map 'you))))))
+	      :for object :being :the :hash-keys :of map
+	      :sum (orbit-count map object))
+	    (length (nset-exclusive-or (build-hierarchy map :san)
+				       (build-hierarchy map :you))))))
